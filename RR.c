3de5c67e2e44b51a396a926c0ef682219f44882f
Paulius Mikalajunas <psypm1@nottingham.ac.uk>
2019-12-13 22:06:43 +0800

    Implementation of the waiting queue

--- RR.c
+++ RR.c
@@ -1,76 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h> 
+#include <string.h>
 #include <math.h>
 #include <ctype.h>
 #include "./LinkedList.h"
 
-// -------------------- CONSTANTS -------------------- 
-int const TIME_QUANTUM = 1; // Fixed time quantum.
+// -------------------- CONSTANTS --------------------
+int const TIME_QUANTUM = 100; // Fixed time quantum.
 int const INCREASE_FACTOR = 2; // Factor by which queue length increases.
-// --------------------------------------------------- 
+// ---------------------------------------------------
 
-// -------------------- VARIABLES -------------------- 
+// -------------------- VARIABLES --------------------
 int queueLength = 64; // May be resized, if exceded.
 int processCount = 0; // Incremented with each input
-// --------------------------------------------------- 
+// ---------------------------------------------------
 
 // -------------- FUNCTION DEFINITIONS ---------------
-void roundRobin(LinkedList *queue);
+void roundRobin(LinkedList *processQueue, LinkedList *waitingQueue);
 void printProcessInfo(Process *process);
 Process *newProcess(int burstTime, int arrivalTime);
 void memCheck(void *ptr);
 bool isInt(char*);
 void printProcessInfo(Process *process);
+int isReady(Node *node, int currentTimeElapsed);
+void discardProcess(Node *node, int timeElapsed);
+void printProcessedNodeData(Node *node, int timeElapsed);
+void printGuidelines();
+void addWaitingNode(LinkedList *waitingQueue, LinkedList *processQueue, int timeElapsed);
 
 // -------------------- FUNCTIONS --------------------
 
-void roundRobin(LinkedList *queue) {
+
+/*
+Tells if the prcoess is ready to be executed.
+Measures arrival time against elapsed time.
+Returns 0 if it's not ready.
+Returns 1 if the process is ready.
+*/
+int isReady(Node *node, int currentTimeElapsed) {
+
+    int arrivalTime = node->process->arrivalTime;
+
+    if(arrivalTime <= currentTimeElapsed) {
+        return 1;
+    }
+    return 0;
+}
+
+/*
+Discards process by assigning completion time and freeing allocated memory.
+Prints process info.
+*/
+void discardProcess(Node *node, int timeElapsed) {
+    node->process->completionTime = timeElapsed - node->process->arrivalTime;
+    printProcessInfo(node->process);
+    free(node);
+}
+
+
+void printProcessedNodeData(Node *node, int timeElapsed) {
+    printf("\nProcess (ID: %d) left CPU, it has been processed for %ds \n",
+           node->process->pId, TIME_QUANTUM);
+    printf("Time left: %ds\n", node->process->burstTime);
+    printf("Process spent %ds in total\n", node->process->timeSpentProcessing);
+    printf("Elapsed time: %ds\n", timeElapsed);
+}
+
+// Checks if the current process is ready to be discarded from waiting queue.
+// If arrival time meets the current time, process gets removed and added to processing queue.
+void addWaitingNode(LinkedList *waitingQueue, LinkedList *processQueue, int timeElapsed) {
+
+    Node *candidateNode = peek_head_linked_list(waitingQueue);
+    if(candidateNode && candidateNode->process->arrivalTime <= timeElapsed) {
+        candidateNode = remove_head_linked_list(waitingQueue);
+        append_linked_list(processQueue, candidateNode->process);
+        printf("\nNode (ID: %d) was added from the waiting queue\n",
+                candidateNode->process->pId);
+        printf("Time elapsed: %d", timeElapsed);
+    }
+}
+
+void roundRobin(LinkedList *processQueue, LinkedList *waitingQueue) {
 
     int timeElapsed = 0;
 
     // While process queue is not empty.
-    while(!linked_list_empty(queue)) {
-
-        // Check if a process is ready to be executed (arrival time)
-
+    while(!linked_list_empty(processQueue)) {
 
         // Getting the head node of the queue.
-        Node *node = remove_head_linked_list(queue);
+        Node *node = remove_head_linked_list(processQueue);
 
+        // Subtracting TIME_QUANTUM from burst time gives us process' remaining time.
         int burstTimeTemp = node->process->burstTime;
         burstTimeTemp -= TIME_QUANTUM;
 
         // Waiting time is equal to overall time elapsed minus the time current process spent executing.
         node->process->waitingTime = timeElapsed - node->process->timeSpentProcessing;
 
-        // Negative burst time means process didn't meet time quantum.
-        if(burstTimeTemp >= 0) {
-          timeElapsed += TIME_QUANTUM;
-          node->process->timeSpentProcessing += TIME_QUANTUM;
-          // Update process burst time by subtracting the time it has been executing.
-          node->process->burstTime -= TIME_QUANTUM; 
-        } else {
-          timeElapsed += abs(TIME_QUANTUM + burstTimeTemp);
-          node->process->timeSpentProcessing += abs(TIME_QUANTUM + burstTimeTemp);
+        // Negative burst time means process finished executing early or exactly on time.
+        // Positive burst time means that the process is not finished.
+        if(burstTimeTemp > 0) {
+            timeElapsed += TIME_QUANTUM;
 
+            // Add the node from the waiting queue (if it's ready).
+            addWaitingNode(waitingQueue, processQueue, timeElapsed);
+
+            node->process->timeSpentProcessing += TIME_QUANTUM;
+            // Update process burst time by subtracting the time it has been executing.
+            node->process->burstTime -= TIME_QUANTUM;
+
+            printProcessedNodeData(node, timeElapsed);
+        } else {
+            int executionTime = abs(TIME_QUANTUM + burstTimeTemp);
+            timeElapsed += executionTime;
+
+            // Add the node from the waiting queue (if it's ready).
+            addWaitingNode(waitingQueue, processQueue, timeElapsed);
+
+            node->process->timeSpentProcessing += executionTime;
+
+            discardProcess(node, timeElapsed);
+            continue;
         }
 
-
-        // Remove process, if it finished executing.
-        if(burstTimeTemp <= 0) {
-            node->process->completionTime = timeElapsed - node->process->arrivalTime;
-
-            printProcessInfo(node->process);
-
-            free(node);
-            continue;
-        } 
-
-
         // Otherwise, put process to the end of the queue.
-        append_linked_list(queue, node->process);
+        append_linked_list(processQueue, node->process);
     }
 
 }
@@ -83,6 +138,7 @@ void printProcessInfo(Process *process) {
     printf("    - Turn-around time: (%d)\n", process->turnAroundTime);
     printf("    - Waiting time: (%d)\n", process->waitingTime);
     printf("    - Burst time: (%d)\n", process->burstTime);
+    printf("    - Arrival time: (%d)\n", process->arrivalTime);
     printf("    - Time Spent Processng: (%d)\n", process->timeSpentProcessing);
     printf("__________________________________________\n");
 }
@@ -116,49 +172,61 @@ void memCheck(void *ptr) {
 // Check if input string contains all integers.
 // Return true if all digits, false if not.
 bool isInt(char* input) {
-  for(int i = 0; i < strlen(input); i++) {
-    if(!isdigit(input[i])) {
-      return false;
+    for(int i = 0; i < strlen(input); i++) {
+        if(!isdigit(input[i])) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
-  
+
+void printGuidelines() {
+    printf("(ERROR) Invalid parameters.\n");
+    printf("Should be in the format of { (Burst time 1) (Arrival time 1) (Burst time 2) (Arrival time 2) ... }\n");
+    printf("For example: RR 54 32 2 32 12 2\n");
+}
+
+bool isEvenNumberOfArguments(int argc) {
+    return (argc - 1) % 2 != 0;
+}
+
 int main(int argc, char *argv[]) {
 
-  	// Allocate memory for process queue.
-  	LinkedList *processQueue = initialise_linked_list();
-
-    // If not even number of inputs.
-    if((argc - 1) % 2 != 0) {
-        printf("(ERROR) Invalid parameters.\n");
-        printf("Should be in the format of { (Burst time 1) (Arrival time 1) (Burst time 2) (Arrival time 2) ... }\n");
-        printf("For example: RR 54 32 2 32 12 2\n");
+    if(isEvenNumberOfArguments(argc)) {
+        printGuidelines();
         return 1;
     }
 
-    // check all int 
+    // Process queue will store all the processes without arrival time.
+    LinkedList *processQueue = initialise_linked_list();
+    // Waiting queue will store all the processes with arrival time.
+    LinkedList *waitingQueue = initialise_linked_list();
+
+    // Check if every entry is an integer.
     for(int i = 1; i < argc; i++) {
-      if(!isInt(argv[i])) {
-        printf("(ERROR) All inputs need to be integers");
-        return 1;
-      }
+        if(!isInt(argv[i])) {
+            printf("(ERROR) All inputs need to be integers");
+            return 1;
+        }
     }
-    
-    // Append all inputs to linked list.
+
+    // Processes with arrival time goes to waiting queue, ones without to processing queue.
     for(int i = 1; i < argc; i += 2) {
         char *ptr;
-        int burstTime = strtol(argv[i], &ptr, 10);
-        int arrivalTime = strtol(argv[i + 1], &ptr, 10);
-        
-        append_linked_list(processQueue, newProcess(burstTime, arrivalTime));
+        int burstTime = (int) strtol(argv[i], &ptr, 10);
+        int arrivalTime = (int) strtol(argv[i + 1], &ptr, 10);
+
+        if(arrivalTime == 0) {
+            append_linked_list(processQueue, newProcess(burstTime, arrivalTime));
+        } else {
+            append_linked_list(waitingQueue, newProcess(burstTime, arrivalTime));
+        }
+
     }
 
+    roundRobin(processQueue, waitingQueue);
 
-
-    roundRobin(processQueue);
-    
-    // free process queue memory
+    // Free the allocated memory for both queues.
     free_linked_list(processQueue);
-
+    free_linked_list(waitingQueue);
 }
